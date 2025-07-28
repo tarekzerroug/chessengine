62cdb728d9ebcca7baa4a6ef7df22bf1414c6845
zerroug <zerroug@MacBookPro-de-zerroug.local>
2025-07-27 23:14:25 -0400

    tests

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-for i in range(10):
-    print(i)
-print(i , "end of loop")
+a = 3 
+
+if a < 4:
+    print("a est inférieur à 4")
+    a =  a+ 3 
+    if a >5:
+        print("a est supérieur à 5")
